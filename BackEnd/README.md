23f177a43a645e3b2b192f39f86721be2c4f31b4
kmmanoj <kmmanoj1990@gmail.com>
2017-11-10 14:59:57 +0530

    API doc updated

--- BackEnd/README.md
+++ BackEnd/README.md
@@ -1,13 +1,53 @@
-<h3>DIAGRAMS REQUIRED</h3>
-<ul>
-	<li>Class Diagrams</li>
-</ul>
+<h3>GET /</h3>
+<fieldset>
+	<legend>response</legend>
+	<pre>
+	{
+		connection : success
+	}
+	</pre>
+</fieldset>
+<hr/>
+<h3>GET /auth/&gt;username&lt;/&gt;password&lt;</h3>
+<fieldset>
+	<legend>response</legend>
+	<pre>
+		{
+			status : int,
+			error : string,
+			response :
+			{
+				name : string,
+				username : string,
+				authenticate : boolean
+			}
+		}
+	</pre>
+</fieldset>
+<hr/>
+<h3>POST /register</h3>
+<fieldset>
+	<legend>request</legend>
+	<pre>
+		{
+			name : string,
+			dob : date,
+			username : string,
+			password : string
+		}
+	</pre>
+</fieldset>
+<fieldset>
+	<legend>response</legend>
+	<pre>
+		{
+			status : int,
+			error : string,
+			response : {
+				status : boolean
+			}
+		}
+	</pre>
+</fieldset>
+<hr/>
 
-<h3>BACKEND</h3>
-<p>Software Layers</p>
-<ul>
-	<li>Web service (Python flask)</li>
-	<li>Functionality CodeBase (Middleware : Bussiness Logic)</li>
-	<li>Database ORM Functions</li>
-	<li>Database(Mongo DB)</li>
-</ul>
--- BackEnd/app/BussinessLogic/api.py
+++ BackEnd/app/BussinessLogic/api.py
@@ -15,12 +15,13 @@ def authenticate(username, password):
     response = dict()
     response['status'] = 200
     user_details = driver.get_details(username)
-    response['name'] = user_details['name']
-    response['username'] = user_details['username']
+    response['response'] = dict()
+    response['response']['name'] = user_details['name']
+    response['response']['username'] = user_details['username']
     if user_details['password'] == password:
-        response['authenticate'] = True
+        response['response']['authenticate'] = True
     else:
-        response['authenticate'] = False
+        response['response']['authenticate'] = False
     response['error'] = None
     return response
 
@@ -31,7 +32,8 @@ given the name, date of birth, username and password
 def register(data): 
     response = dict()
     response['status'] = 200
-    response['message'] = driver.add_user(name=data['name'], date_of_birth=data['dob'], username=data['username'], password=data['password'])
+    response['response'] = dict()
+    response['response']['status'] = driver.add_user(name=data['name'], date_of_birth=data['dob'], username=data['username'], password=data['password'])
     response['error'] = None
 
 '''
@@ -40,10 +42,11 @@ function to check if the user with a specific username already exist
 def check_user(username): 
     response = dict()
     response['status'] = 200
+    response = dict()
     if driver.get_details(username)['name'] is None:
-        response['exists'] = False
+        response['response']['exists'] = False
     else:
-        response['exists'] = True
+        response['response']['exists'] = True
     response['error'] = None
     return response
 
@@ -54,7 +57,7 @@ starting from offset and limiting number of records to limit
 def get_transactions(user, offset, limit): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_transactions(user, offset, limit)
+    response['response'] = driver.get_transactions(user, offset, limit)
     response['error'] = None
     return response
 
@@ -64,7 +67,7 @@ function to add a transaction record for the user
 def add_transaction(user, data): 
     response = dict()
     response['status'] = 200
-    response['message'] = driver.add_transaction(user, date=data['date'], type=data['type'], category=data['category'], dealer=data['dealer'], status=data['status'])
+    response['response'] = driver.add_transaction(user, date=data['date'], type=data['type'], category=data['category'], dealer=data['dealer'], status=data['status'])
     response['error'] = None
     return response
 
@@ -74,7 +77,7 @@ function to delete a transaction record for a user given the id
 def delete_transaction(user, doc_id): 
     response = dict()
     response['status'] = 200
-    response['message'] = driver.delete_transaction(user, doc_id)
+    response['response'] = driver.delete_transaction(user, doc_id)
     response['error'] = None
     return response
 
@@ -84,7 +87,7 @@ function to get a specific transaction record given the id
 def get_transaction_by_id(user, doc_id): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_tansaction_by_id(user, doc_id)
+    response['response'] = driver.get_tansaction_by_id(user, doc_id)
     response['error'] = None
     return response
     
@@ -94,7 +97,7 @@ function to update the transaction given the new data
 def update_transactions(user, data): 
     response = dict()
     response['status'] = 200
-    response['message'] = driver.update_transactions(user, doc_id=data['id'], date=data['date'], type=data['type'], category=data['category'], dealer=data['dealer'], status=data['status'])
+    response['response'] = driver.update_transactions(user, doc_id=data['id'], date=data['date'], type=data['type'], category=data['category'], dealer=data['dealer'], status=data['status'])
     response['erorr'] = None
     return response
 
@@ -105,7 +108,7 @@ starting from the offset and limiting the number of output records to limit
 def get_debt_list(user, offset, limit): 
     response = dict() 
     response['status'] = 200
-    response['data'] = driver.get_debt_list(user, offset, limit)
+    response['response'] = driver.get_debt_list(user, offset, limit)
     response['error'] = None
     return response
 
@@ -118,7 +121,7 @@ def update_debt_list(user, id_list):
     count = 0
     for doc_id in id_list:
         count += driver.update_debt(user, doc_id)
-    response['message'] = count
+    response['response'] = count
     response['error'] = None
     return response
 
@@ -129,7 +132,7 @@ starting from the offset and limiting the number of output records to limit
 def get_owe_list(user, offset, limit): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_owe_list(user, offset, limit)
+    response['response'] = driver.get_owe_list(user, offset, limit)
     response['error'] = None
     return response
 
@@ -142,7 +145,7 @@ def update_owe_list(user, offset, limit):
     count = 0
     for doc_id in id_list:
         count += driver.update_owe_list(user, offset, limit)
-    response['message'] = count
+    response['response'] = count
     response['error'] = None
     return response
 
@@ -152,7 +155,7 @@ function to get the list of unique categories for a user
 def get_all_categories(user): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_all_categories(user)
+    response['response'] = driver.get_all_categories(user)
     response['error'] = None
     return response
 
@@ -162,7 +165,7 @@ function to get summary data by category for a user
 def get_summary_by_category(user): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_summary_by_category(user)
+    response['response'] = driver.get_summary_by_category(user)
     response['error'] = None
     return response
 
@@ -172,7 +175,7 @@ function to get summary data by weekdays for a user
 def get_summary_by_weekdays(user): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_summary_by_weekdays(user)
+    response['response'] = driver.get_summary_by_weekdays(user)
     response['error'] = None
     return response
 
@@ -183,7 +186,7 @@ starting from the offset and limiting the number of output records to limit
 def get_credit_transactions(user, offset, limit): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_credit_transactions(user, offset, limit)
+    response['response'] = driver.get_credit_transactions(user, offset, limit)
     response['error'] = None
     return response
 
@@ -194,7 +197,7 @@ starting from the offset and limiting the number of output records to limit
 def get_debit_transactions(user, offset, limit): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_debit_transactions(user, offset, limit)
+    response['response'] = driver.get_debit_transactions(user, offset, limit)
     response['error'] = None
     return response
 
@@ -205,6 +208,6 @@ starting from the offset and limiting the number of output records to limit
 def get_saved_transactions(user, offset, limit): 
     response = dict()
     response['status'] = 200
-    response['data'] = driver.get_saved_transactions(user, offset, limit)
+    response['response'] = driver.get_saved_transactions(user, offset, limit)
     response['error'] = None
     return response
