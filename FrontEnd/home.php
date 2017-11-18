5ee263b7fd6f773bc32386df0cb09b68c053ec5f
kmmanoj <kmmanoj1990@gmail.com>
2017-11-18 14:09:14 +0530

    moved delete handler

--- FrontEnd/home.php
+++ FrontEnd/home.php
@@ -1,5 +1,35 @@
 <?php
 session_start();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  if(isset($_POST['delete']))
+  {
+    $id_list = array();
+    parse_str(file_get_contents("php://input", $id_list));
+    $return_json = array();
+    $count = 0;
+    foreach( $id_list as $id=>$value)
+    {
+        $request_url = "http://localhost:5000/transactions/delete/$id";
+        $request_method = "GET";
+        
+        header("Content-type:application/json");
+        $data = json_encode(file_get_contents("php://input"));
+        $uname   = $_SESSION['uname'];
+        $header  = array("User:$uname", "Content-type:application/json");
+        $request = array('http' => array('method' => $request_method, 'header' => $header, 'content' => $data));
+        $context = stream_context_create($request);
+        $retval  = file_get_contents($request_url, FALSE, $context);    
+        $ret_assoc = json_decode($retval);
+        if($ret_assoc['response']['status'])
+        {
+            $count += 1;
+        }
+    }
+    echo $count;
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
