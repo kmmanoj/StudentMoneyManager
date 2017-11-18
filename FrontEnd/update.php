f4767ccf17a88ba9deedcb4f86d90728da1a9037
kmmanoj <kmmanoj1990@gmail.com>
2017-11-18 21:50:12 +0530

    updated update.php

--- FrontEnd/update.php
+++ FrontEnd/update.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
-//header('Location:home.php');
+header('Location:home.php',"Content-type:application/json");
 $data = array();
 parse_str(file_get_contents("php://input"), $data);
-var_dump($data);
 $id = $data['id'];
 unset($data['id']);
 $request_url    = "http://localhost:5000/transactions/edit/$id";
 $request_method = "POST";
-header("Content-type:application/json");
 $data = json_encode($data, true);
-echo ($data);
 $uname   = $_SESSION['uname'];
 $header  = array("User:$uname", "Content-type:application/json");
 $request = array('http' => array('method' => $request_method, 'header' => $header, 'content' => $data));
