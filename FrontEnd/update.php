121095834a35d0ea642d27a4dce36903385de1f6
kmmanoj <kmmanoj1990@gmail.com>
2017-11-18 15:03:41 +0530

    update updated.php

--- FrontEnd/update.php
+++ FrontEnd/update.php
@@ -1,16 +1,18 @@
 <?php
 session_start();
+header('Location:home.php');
+$data = array();
+parse_str(file_get_contents("php://input"), $data);
+$id = $data['id'];
+unset($data['id']);
+$request_url = "http://localhost:5000/transactions/edit/$id";
+$request_method = "GET";
+
 header("Content-type:application/json");
-
-$data = file_get_contents("php://input");
-$send = json_encode($data);
-$temp = json_decode($data, true);
-
-//echo $temp['id'];
+$data    = json_encode($data);
 $uname   = $_SESSION['uname'];
 $header  = array("User:$uname", "Content-type:application/json");
-$request = array('http' => array('method' => "POST", 'header' => $header, 'content' => $data));
+$request = array('http' => array('method' => $request_method, 'header' => $header, 'content' => $data));
 $context = stream_context_create($request);
-$retval  = file_get_contents("http://localhost:5000/transactions/edit/".$temp['id'], FALSE, $context);
-echo $retval;
+$retval  = file_get_contents($request_url, FALSE, $context);
 ?>
