a4f293943da5fd63f186605b9b9f0e369bc5404c
kmmanoj <kmmanoj1990@gmail.com>
2017-11-18 13:11:37 +0530

    added get all php file

---
+++ FrontEnd/getAll.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$request_url = "http://192.168.254.101:5000/transactions/all/0/1000";
+$request_method = "GET";
+
+header("Content-type:application/json");
+$data = json_encode(file_get_contents("php://input"));
+$uname   = $_SESSION['uname'];
+$header  = array("User:$uname", "Content-type:application/json");
+$request = array('http' => array('method' => $request_method, 'header' => $header, 'content' => $data));
+$context = stream_context_create($request);
+$retval  = file_get_contents($request_url, FALSE, $context);
+echo ($retval);
+?>
